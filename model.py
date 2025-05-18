e501a86ea70ca646341034b74de63407071d987d
Ahmed Hodiani <a.hodiani@hotmail.com>
2025-05-18 15:16:13 +0300

    feat: connected the new data

--- model.py
+++ model.py
@@ -17,20 +17,31 @@ def retrieve_context(query, top_k=1):
     contexts = []
 
     for hit in hits[0]:
-        if hit['score'] > 0.40:
-            print("score: ", hit['score'], "context: ", rag_data[hit['corpus_id']])
+        print("score: ", hit['score'], "context: ", rag_data[hit['corpus_id']])
+        if hit['score'] > 0.50:
             contexts.append(rag_data[hit['corpus_id']])
     return contexts
 
 
-def generate_answer(contexts, question):
+def generate_answer(contexts = [], question = ''):
     prompt = ""
+
+    if question.strip() == "":
+        return
+
     for context in contexts:
         qna = context.split('?')
-        prompt += f"Question: {qna[0]}\nAnswer: {qna[1]}\n"
+        prompt += f"Question: {qna[0]}?\nAnswer: {qna[1]}\n"
 
     prompt += f"Question: {question}\nAnswer: "
-    result = pipe(prompt, do_sample=False, temperature=0.5)
+
+    print("==================================")
+    print("==================================")
+    print(prompt)
+    print("==================================")
+    print("==================================")
+
+    result = pipe(prompt, max_new_tokens=200, temperature=0.5)
     text = result[0]['generated_text']
 
     answer = text.split("Answer:")[1].split("Question:")[0].strip()
