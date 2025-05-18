7628add260b66a9ade056414f8314c3c24f865f1
Ahmed Hodiani <a.hodiani@hotmail.com>
2025-05-18 10:09:00 +0300

    fix: CORS and system prompt

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from flask import Flask, request, jsonify
 from model import generate_answer
 from sql import init_db, insert_message, get_latest_messages
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+SYSTEM_PROMPT = """
+you are a helpful assistant. you're responses should be short and to the point.
+you will be provided the last 5 messages between the user and the assistant.
+if the new question is a continuation of the conversation use the previous messages. to know the context.
+                        
+The last message is the new question
+"""
 
 @app.route('/ask', methods=['POST'])
 def ask():
     data = request.json
     question = data.get('question', '')
-    system_prompt = data.get('system_prompt', 'you are a helpful assistant.')
+
+    if not question or question.strip() == '':
+        return jsonify({'error': 'No question provided'})
+
     print(question)
 
-
-    messages = get_latest_messages(10)
-    messages.insert(0, {"role": "system", "content": system_prompt})
+    messages = get_latest_messages(5)
+    messages.insert(0, {"role": "system", "content": SYSTEM_PROMPT})
     messages.append({"role": "user", "content": question})
 
     print(messages)
